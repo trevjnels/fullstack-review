119ed9dc425ce51ef6e993e13883da74b135de74
trevjnels <trevorjohnnels@gmail.com>
2019-05-10 12:11:43 -0700

    finished schema design

--- database/index.js
+++ database/index.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  idRepo: Number, //github's repo ID - our main unique id
+    username: String, // username from post request from client / confirmed in sucessful git GET. in github this will be data.owner.login
+    idUser: Number, //  github USERID data.owner.id
+    stargazers_count: Number,
+    html_url: String,  // link to the repo  //data.html_url
+    nameRepo: String// repoName // data.name
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
